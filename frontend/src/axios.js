8ba3c7fe9adba521194bbf7ea06745a7fa66991e
Raj <intajchoudhury@gmail.com>
2024-01-28 19:00:22 +0530

    Added vercel api

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // The API (Cloud Function) URL
   // baseURL: "http://127.0.0.1:5001/challenge-809dc/us-central1/api",
-  baseURL: "http://127.0.0.1:5001",
+  baseURL: "https://amazon-clone-one-red.vercel.app/",
 });
 
 export default instance;
