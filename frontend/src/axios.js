268145664dd6e6e4a01a09f38e6bb7773b13d153
Raj <intajchoudhury@gmail.com>
2024-01-30 00:25:18 +0530

    Updated axios in frontend to connect to backend at vercel deployed

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   // The API (Cloud Function) URL
   // baseURL: "http://127.0.0.1:5001/challenge-809dc/us-central1/api",
-  baseURL: "https://amazon-clone-one-red.vercel.app/",
   // baseURL: "http://127.0.0.1:5001",
+  baseURL: "https://backend-new-amazon-clone-project.vercel.app/",
 });
 
 export default instance;
